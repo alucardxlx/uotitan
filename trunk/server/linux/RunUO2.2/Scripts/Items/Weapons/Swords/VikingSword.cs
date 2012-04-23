cb15a95cce2d5d55fd518fc79cd2b99010b3e000
igcogi@gmail.com <igcogi@gmail.com@d4d53274-77be-0acb-e1fb-cd7f267e8fc7>
2012-04-23 19:37:47 +0000

    Modificando armas

--- trunk/server/linux/RunUO2.2/Scripts/Items/Weapons/Swords/VikingSword.cs
+++ trunk/server/linux/RunUO2.2/Scripts/Items/Weapons/Swords/VikingSword.cs
@@ -10,7 +10,7 @@ namespace Server.Items
 		public override WeaponAbility PrimaryAbility{ get{ return WeaponAbility.CrushingBlow; } }
 		public override WeaponAbility SecondaryAbility{ get{ return WeaponAbility.ParalyzingBlow; } }
 
-		public override int AosStrengthReq{ get{ return 40; } }
+		/*public override int AosStrengthReq{ get{ return 40; } }
 		public override int AosMinDamage{ get{ return 15; } }
 		public override int AosMaxDamage{ get{ return 17; } }
 		public override int AosSpeed{ get{ return 28; } }
@@ -19,18 +19,35 @@ namespace Server.Items
 		public override int OldStrengthReq{ get{ return 40; } }
 		public override int OldMinDamage{ get{ return 6; } }
 		public override int OldMaxDamage{ get{ return 34; } }
-		public override int OldSpeed{ get{ return 30; } }
-
+		public override int OldSpeed{ get{ return 30; } }*/
+		
+		public override int InitMinHits{ get{ return 31; }}
+		public override int InitMaxHits{ get{ return 100; }}
+		
 		public override int DefHitSound{ get{ return 0x237; } }
 		public override int DefMissSound{ get{ return 0x23A; } }
-
-		public override int InitMinHits{ get{ return 31; } }
-		public override int InitMaxHits{ get{ return 100; } }
-
+		
+		
+		public override int uotMinMinDamage{ get{return 1;}}
+		public override int uotMinMaxDamage{ get{return 2;}}
+				
+		public override int uotMaxMinDamage{ get{return 1;}}
+		public override int uotMaxMaxDamage{ get{return 2;}}
+		
+		[CommandProperty(AccessLevel.Counselor, AccessLevel.GameMaster)]
+		public override float uotSpeed{ get{return 1f; }}
+		
+		
 		[Constructable]
-		public VikingSword() : base( 0x13B9 )
-		{
+		public VikingSword(int level) : base( 0x13B9 ){
 			Weight = 6.0;
+			uotLevel = level;
+		}
+		
+		[Constructable]
+		public VikingSword() : base( 0x13B9 ){
+			Weight = 6.0;
+			uotLevel = 0;
 		}
 
 		public VikingSword( Serial serial ) : base( serial )
