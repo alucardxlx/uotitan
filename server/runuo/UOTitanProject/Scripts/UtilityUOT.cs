31e7cd0fe92cbddd1213810a2ceece4a79abb83b
igcogi <igcogi@gmail.com>
2012-04-29 16:20:15 +0000

    Sistema de combate 	* Cálculo de la probabilidad de dar en cada una de las zonas (dependencia con la velocidad del arma) 	* Cálculo de la probabilidad de asestar crítico Clase UtilityUOT 	* Función check<T> Al que le pasas un diccionario con claves de tipo T y valor tipo float que representa su  probabilidad. Devuelve la clave de tipo T elegida al azar de acuerdo a su probabilidad. 	* Función checkChance() Comprueba una probabilidad.

---
+++ server/runuo/UOTitanProject/Scripts/UtilityUOT.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+
+namespace Server
+{
+	public static class UtilityUOT
+	{
+		public static T check<T>(Dictionary<T,float> collection) {
+			float value = (float) Utility.RandomDouble();
+			
+			/* Correction */
+			value = (float) Math.Round(value, 4);
+			float rest = -0.0001f;
+			
+			foreach (KeyValuePair<T, float> kvp in collection) {
+				if(value < (kvp.Value/100)+rest) {
+					return kvp.Key;
+				}
+				rest += kvp.Value/100;
+			}
+			
+			T[] keys = new T[collection.Keys.Count];
+			
+			collection.Keys.CopyTo(keys,0);
+			
+			return keys[keys.Length-1];
+		}
+		
+		public static bool checkChance(float chance){
+			float value = (float) Utility.RandomDouble();
+			
+			/* Correction */
+			value = (float) Math.Round(value, 4);
+			float rest = -0.0001f;
+			
+			if(value < (kvp.Value/100)+rest) return true;
+			else return false;
+		}
+	}
+}
+
